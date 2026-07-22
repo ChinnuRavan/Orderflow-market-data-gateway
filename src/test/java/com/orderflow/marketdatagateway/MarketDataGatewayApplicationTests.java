9c8edf88af89258ce87bf5c13053feb7c509cbae
Bezawada Naga Rajasri <bezawadaammaji@gmail.com>
2026-07-22 10:41:19 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/orderflow/marketdatagateway/MarketDataGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderflow.marketdatagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
