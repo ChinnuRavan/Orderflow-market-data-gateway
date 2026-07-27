50d5d368a7ba84292bdb499997104379db4dfdc9
Bezawada Naga Rajasri <bezawadaammaji@gmail.com>
2026-07-27 16:34:05 +0530

    Added logging to MarketDataService

--- src/main/java/com/orderflow/marketdatagateway/service/MarketDataService.java
+++ src/main/java/com/orderflow/marketdatagateway/service/MarketDataService.java
@@ -2,15 +2,22 @@ package com.orderflow.marketdatagateway.service;
 
 import com.orderflow.marketdatagateway.exception.MarketDataNotFoundException;
 import com.orderflow.marketdatagateway.model.MarketData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class MarketDataService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MarketDataService.class);
+
     public Mono<MarketData> getMarketData(String symbol) {
 
+        logger.info("Fetching market data for symbol: {}", symbol);
+
         if (symbol.equalsIgnoreCase("INVALID")) {
+            logger.error("Market data not found for symbol: {}", symbol);
             throw new MarketDataNotFoundException(symbol);
         }
 
